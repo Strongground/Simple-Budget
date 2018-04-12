e2a67362e89f9973f983ebda507189f0d2a5f7e5
Noël Ströhmer-Lohfink <noel@strongground.de>
2018-04-12 07:22:46 +0200

    Fix #4 and added context_processor for access to settings constants
    Context processor is not yet used but easily extended. Redirect to login page after 2.5 seconds on logout page via JS added.

--- savings/static/js/budget.js
+++ savings/static/js/budget.js
@@ -1,6 +1,14 @@
-(function ($) {
-  $.fn.initGUI = function () {
-    // init dom
-  }
-}(jQuery))
-console.log('gui module v4 initialized')
+// register timeouts and execute actions
+if ($('.alert').length >= 1) {
+  window.setTimeout(() => {
+    $('.alert').alert('close')
+  }, 2000)
+}
+let isLogoutPage = $('#logout-page')
+let logoutRedirectTarget = $('#logout-page').attr('data-login-target')
+let logoutRedirectURL = window.location.protocol + '//' + window.location.host + logoutRedirectTarget
+if (isLogoutPage) {
+  window.setTimeout(() => {
+    window.location = logoutRedirectURL
+  }, 2500)
+}
